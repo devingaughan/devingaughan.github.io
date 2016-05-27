76ab013fd60b601c77cb8c335644457cc5c4b74a
unknown <devin@zovkov.com>
2016-05-27 11:19:46 -0700

    changed color and added carsoul

--- public_html/js/Odis.js
+++ public_html/js/Odis.js
@@ -2,6 +2,9 @@
     $(document).ready(function(){
       $('.slider').slider({full_width: true});
     });
- 
+  $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+        
  
  
